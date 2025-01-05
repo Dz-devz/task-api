379beef1233562206f0e969e8611dddaeeab5653
Dz.dev <darwinbjordan@gmail.com>
2025-01-05 21:59:33 +0800

    progress: added a sample CRUD in my api

--- src/api/chat-app.ts
+++ src/api/chat-app.ts
@@ -1,7 +1,20 @@
 import { Hono } from "hono";
 
-export const chatRoute = new Hono().get("/getMessage", async (c) => {
-  console.log("Hello from Hono!");
+export const chatRoute = new Hono()
+  .get("/getMessage", async (c) => {
+    console.log("Hello from Hono!");
 
-  return c.json("Hello from hono");
-});
+    return c.json("Hello from hono");
+  })
+  .post("/create-message", async (c) => {
+    const response = c.req.json;
+    return c.json(response);
+  })
+  .put("/update-message/:id", async (c) => {
+    const params = c.req.param("id");
+    return c.json(params);
+  })
+  .delete("/delete-message/:id", async (c) => {
+    const params = c.req.param("id");
+    return c.json(params);
+  });
