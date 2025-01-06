0f2b7b9eb7eb711d86126a35018010ebbdffa658
Dz.dev <darwinbjordan@gmail.com>
2025-01-06 14:30:00 +0800

    progress: deleted some files and stick to one file and added openapi swagger

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { chatRoute } from "./api/chat";
-
-const app = new Hono();
-
-const apiRoutes = app.basePath("/api").route("/chat-app", chatRoute);
-
-export default app;
-export type ApiRoutes = typeof apiRoutes;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,63 @@
 import { serve } from "@hono/node-server";
+import { swaggerUI } from "@hono/swagger-ui";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { config } from "dotenv";
-import app from "./app";
+import { ChatSchema } from "./api/schemas/chat-schema";
+
+const app = new OpenAPIHono();
+config();
+
+const getRoute = createRoute({
+  method: "get",
+  path: "/getMessage",
+  summary: "Retrieve a message",
+  description: "Retrieve the message of the users.",
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: ChatSchema,
+        },
+      },
+      description: "Retrieve Message",
+    },
+  },
+});
+
+app.openapi(getRoute, (c) => {
+  return c.json({
+    chat_id: 1,
+    chat_type: "PRIVATE" as "PRIVATE" | "GROUP",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    UserChats: [],
+    Messages: [],
+    message: "Messages Retrieved",
+  });
+});
+
+app.get(
+  "/",
+  swaggerUI({
+    url: "/openapi.json",
+  })
+);
+
+app.doc("/openapi.json", {
+  info: {
+    title: "An API",
+    version: "v1",
+  },
+  openapi: "3.1.0",
+});
+
+app.route("/", app);
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 serve({
   fetch: app.fetch,
+  port: PORT,
 });
-config();
-
-const PORT = process.env.PORT || 3000;
 
 console.log(`Server is running on http://localhost:${PORT}`);
